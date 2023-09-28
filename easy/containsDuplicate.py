3d9e39bfa550e932cf1814ca8000334dd0cd85cc
aryawdy <aryawdy16@gmail.com>
2023-09-28 21:53:05 +0700

    contains duplicate improve time complexity

--- easy/containsDuplicate.py
+++ easy/containsDuplicate.py
@@ -1,10 +1,23 @@
 # Time : O(nlogn)
 # Space : O(1)
+# def containsDuplicate(nums):
+#     sortedNums=sorted(nums)
+#     for i in range(len(sortedNums) - 1):
+#         if sortedNums[i] == sortedNums[i+1]:
+#             return True
+#     return False
+
+
+
+
+
+# Time : O(n)
+# Space : O(n)
 def containsDuplicate(nums):
-    sortedNums=sorted(nums)
-    for i in range(len(sortedNums) - 1):
-        if sortedNums[i] == sortedNums[i+1]:
-            return True
+    seen = set()
+    for x in nums:
+      if x in seen: return True
+      seen.add(x)
     return False
 
 
